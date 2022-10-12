58c47f75716e316a5502646a3a88cec5e3d572d9
Alex Cuenca-Parra <alejandrocuencaparra@gmail.com>
2022-10-12 13:38:57 +0200

    add get user
    Signed-off-by: Alex Cuenca-Parra <alejandrocuencaparra@gmail.com>

--- app/main.py
+++ app/main.py
@@ -87,6 +87,17 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 # USERS CRUD
 
+@app.get("/users/{id}", response_model=schemas.UserResponse)
+def get_user(id: int, db: Session = Depends(get_db)):
+
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"user {id} does not exist")
+
+    return user
+
+
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
