b2ba744a4ba4dbb44ddca49e685c5189a0f5a401
Alex Cuenca-Parra <alejandrocuencaparra@gmail.com>
2022-11-07 13:41:45 +0100

    refactor tests DB code

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,50 +1,5 @@
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-import pytest
-# from alembic import command
-
-from app.main import app
 from app import schemas
-from app.config import settings
-from app.database import get_db, Base
-
-# SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:1234@localhost:5432/fastapi_test"
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_password}@{settings.db_host_uri}_test"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
-TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine)
-
-
-@pytest.fixture
-def session():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    # another option, with alembic instead of sqlalchemy:
-    # command.downgrade("base")
-    # command.upgrade("head")
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@pytest.fixture
-def client(session):
-    # Dependency
-    def override_get_db():
-        try:
-            yield session
-        finally:
-            session.close()
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
-
+from .database import client, session # session fixture is also needed as client makes use of it
 
 def test_root(client):
     response = client.get("/")
